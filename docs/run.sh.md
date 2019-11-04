2b68158f4cbda1ee8a0ff5fc6d6300c0a022c0d8
0x5c <0x5c.dev@gmail.com>
2019-11-04 05:09:54 -0500

    run.sh: Added argument passing. Also now POSIX. - You can now pass arguments to the bot through 'run.sh'! - Now only requires a POSIX shell (instead of bash).   - Any failure to do so is accidental.
    Fixes #3

--- docs/run.sh.md
+++ docs/run.sh.md
@@ -1,8 +1,5 @@
 # Quick bot, No pain / `run.sh`
 
-
-## Usage
-
 > **Note:** This wrapper assumes that the bot is  
 > a) in the same directory  
 > and  
@@ -10,14 +7,19 @@
 >
 > Modify it to fit your needs.
 
+## Usage
+
+
 ```none
-$ run.sh [--pass-errors]
+$ run.sh [--pass-errors] [-- [bot arguments]]
 ```
 
 ### Command line arguments
 
-- `--pass-errors`: Exits on error from the bot, instead of restarting it.  
+- `--pass-errors` : ­ ­ Exits on error from the bot, instead of restarting it.  
     *Exits with the error code from the bot.*
+- `--` : ­ ­ Stops argument parsing. Any further argument will be passed to the bot.
+    > **Note** This is required to be able to pass arguments to the bot.
 
 ### Environment variables
 
@@ -28,7 +30,14 @@ $ run.sh [--pass-errors]
 
 ## Changelog
 
-```none
+```md
+## [1.1.0] - 2019-11-04
+### Added
+- Argument passing
+
+### Changed
+- No longer requires `bash`
+
 ## [1.0.0] - 2019-10-06
 ### Added
 - Initial release
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # A wrapper script for painless discord bots.
-# v1.0.0
+# v1.1.0
 # Copyright (c) 2019 0x5c
 # Released under the terms of the MIT license.
 # Part of:
@@ -9,14 +9,24 @@
 
 
 # If $BOTENV is not defined, default to 'botenv'
-if [[ -z ${BOTENV+x} ]]; then
+if [ -z "$BOTENV" ]; then
     BOTENV='botenv'
 fi
 
-# Argument handling     # ? TODO: Argument passing ?
-if [[ $1 == '--pass-errors' ]]; then 
-    _PASS_ERRORS=1
-fi
+# Argument handling
+_PASS_ERRORS=0
+while [ ! -z "$1" ]; do
+    case $1 in
+        --pass-errors)
+            _PASS_ERRORS=1
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+    shift
+done
 
 
 # A function called when the bot exits to decide what to do
@@ -31,7 +41,7 @@ code_handling() {
             return      # The bot whishes to be restarted (returns to the loop).
             ;;
         *)
-            if [[ $_PASS_ERRORS -eq 0 ]]; then    # The bot crashed and:
+            if [ $_PASS_ERRORS -eq 0 ]; then    # The bot crashed and:
                 echo "$_message: restarting"
                 return      # ...we should return to the loop to restart it.
             else
@@ -47,7 +57,7 @@ echo "$0: Starting bot..."
 
 # The loop
 while true; do
-    ./$BOTENV/bin/python3 main.py
+    ./$BOTENV/bin/python3 main.py $@
     err=$?
     _message="$0: The bot exited with [$err]"
     code_handling
