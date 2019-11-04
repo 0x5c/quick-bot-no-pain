7203b79850f15e49ab9fb0cbf40418ced9e209e2
0x5c <0x5c.dev@gmail.com>
2019-11-04 08:57:29 -0500

    run.sh: Added --no-botenv - The script can now use an arbitrary python executable,   to be specified in $PYTHON_BIN.
    Fixes #7

--- docs/run.sh.md
+++ docs/run.sh.md
@@ -11,13 +11,16 @@
 
 
 ```none
-$ run.sh [--pass-errors] [-- [bot arguments]]
+$ run.sh [--pass-errors] [--no-botenv] [-- [bot arguments]]
 ```
 
 ### Command line arguments
 
 - `--pass-errors` : ­ ­ Exits on error from the bot, instead of restarting it.  
     *Exits with the error code from the bot.*
+
+- `--no-botenv` : ­ ­ Allows usage of an arbitrary python executable, as specified in `PYTHON_BIN`.
+
 - `--` : ­ ­ Stops argument parsing. Any further argument will be passed to the bot.
     > **Note** This is required to be able to pass arguments to the bot.
 
@@ -27,10 +30,18 @@ $ run.sh [--pass-errors] [-- [bot arguments]]
 
 - `BOTENV`=`botenv`: The name of the venv to use.
 
+- `PYTHON_BIN`=`python3.7`: Defines the python executable to use.  
+    > A full path may be given.
+
 
 ## Changelog
 
 ```md
+## [1.2.0] - 2019-11-04
+### Added
+- Ability to use arbitrary python executable.
+- Environment variable to specify the executable.
+
 ## [1.1.0] - 2019-11-04
 ### Added
 - Argument passing
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # A wrapper script for painless discord bots.
-# v1.1.0
+# v1.2.0
 # Copyright (c) 2019 0x5c
 # Released under the terms of the MIT license.
 # Part of:
@@ -13,12 +13,17 @@ if [ -z "$BOTENV" ]; then
     BOTENV='botenv'
 fi
 
+
 # Argument handling
 _PASS_ERRORS=0
+_NO_BOTENV=0
 while [ ! -z "$1" ]; do
     case $1 in
         --pass-errors)
             _PASS_ERRORS=1
+            ;;
+        --no-botenv)
+            _NO_BOTENV=1
             ;;
         --)
             shift
@@ -27,6 +32,12 @@ while [ ! -z "$1" ]; do
     esac
     shift
 done
+
+
+# If $PYTHON_BIN is not defined, default to 'python3.7'
+if [ $_NO_BOTENV -eq 1 -a -z "$PYTHON_BIN" ]; then
+    PYTHON_BIN='python3.7'
+fi
 
 
 # A function called when the bot exits to decide what to do
@@ -57,7 +68,11 @@ echo "$0: Starting bot..."
 
 # The loop
 while true; do
-    ./$BOTENV/bin/python3 main.py $@
+    if [ $_NO_BOTENV -eq 1 ]; then
+        "$PYTHON_BIN" main.py $@
+    else
+        ./$BOTENV/bin/python3 main.py $@
+    fi
     err=$?
     _message="$0: The bot exited with [$err]"
     code_handling
